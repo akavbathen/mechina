2e73deb674f73366761a213e2e073de931ce5191
progovoy <p.rogovoy@gmail.com>
2021-08-18 13:01:57 +0300

    תרגיל

--- js/main2.js
+++ js/main2.js
@@ -1,25 +1,40 @@
-alcoholAgeCheck();
-alcoholAgeCheck();
-alcoholAgeCheck();
 function alcoholAgeCheck() {
-     var userName = prompt('What is your name?');
-     var age = prompt('What is your age?');
+    var userName = prompt('What is your name?');
+    var age = prompt('What is your age?');
     if (age >= 18) {
         console.log(
             'Welcome ' +
-                userName +
-                ', you are ' +
-                age +
-                ', so you are old enough to buy Beer'
+            userName +
+            ', you are ' +
+            age +
+            ', so you are old enough to buy Beer'
         );
     } else {
         console.log(
             'We are sorry, ' +
-                userName +
-                ', you are ' +
-                age +
-                ', and it means that you are too young for buying Beer'
+            userName +
+            ', you are ' +
+            age +
+            ', and it means that you are too young for buying Beer'
         );
     }
-    
+
 }
+function drink() {
+    var drink_name = prompt('what your favorite drink?');
+    console.log('Enjoy your ' + drink_name +'!');
+}
+
+
+
+var score = 1;
+function multTen() {
+score = score * 10;
+console.log(score);
+
+}
+
+multTen();
+//alcoholAgeCheck();
+//alcoholAgeCheck();
+//alcoholAgeCheck();
