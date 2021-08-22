1c9279282f88b3990f9def24fb3d2452363abecd
progovoy <p.rogovoy@gmail.com>
2021-08-22 15:06:59 +0300

    שיעור 3

--- js/main2.js
+++ js/main2.js
@@ -1,3 +1,10 @@
+//alcoholAgeCheck();
+//alcoholAgeCheck();
+//alcoholAgeCheck();
+//calculateMult();
+ //resetScore();
+ //multTen();
+
 function alcoholAgeCheck() {
     var userName = prompt('What is your name?');
     var age = prompt('What is your age?');
@@ -40,11 +47,13 @@ function resetScore() {
     if (score > 100000) {
         score = 1;
         console.log(score);
-       
+
     }
 }
-resetScore();
-multTen();
-//alcoholAgeCheck();
-//alcoholAgeCheck();
-//alcoholAgeCheck();
+function calculateMult() {
+    var use1 = prompt('give me number pleas?');
+    var use2 = prompt('give me number pleas?');
+    var use3 = prompt('give me number pleas?');
+    console.log(' their multi plication of 3 number '+ use1 * use2 * use3);
+  }
+
