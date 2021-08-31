c23d2050aec273d97fd684645ddb3c19213a567c
Pavel Rogovoy <p.rogovoy@gmail.com>
2021-08-31 19:42:15 +0300

    changeTextSize

--- js/main3.js
+++ js/main3.js
@@ -2,5 +2,15 @@ console.log('day 4');
 function changeTextColor() {
     var el_ball1 = document.querySelector('.ball1');
     console.log(el_ball1);
-    el_ball1.style.color = 'red';
+    el_ball1.style.color = 'green';
+}
+function changeTextSize() {
+    var el_ball1 = document.querySelector('.ball1');
+
+    var size = prompt('give me size pleas?');
+    if (size > 50) {
+        console.log('Too big' + '!');
+    } else {
+        el_ball1.style.fontSize = size + 'px';
+    }
 }
