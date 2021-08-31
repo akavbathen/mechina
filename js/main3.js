0dc767df42d84f996b123a7e6e178a93c2eb1e68
progovoy <p.rogovoy@gmail.com>
2021-08-31 17:11:24 +0300

    change to red

--- js/main3.js
+++ js/main3.js
@@ -2,5 +2,5 @@ console.log('day 4');
 function changeTextColor() {
     var el_ball1 = document.querySelector('.ball1');
     console.log(el_ball1);
-    el_ball1.style.color = 'green';
+    el_ball1.style.color = 'red';
 }
