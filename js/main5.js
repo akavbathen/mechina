d0187227ede05c8bc86d7c2fc56974219bfa2c6a
progovoy <p.rogovoy@gmail.com>
2021-09-01 22:13:40 +0300

    Update main5.js

--- js/main5.js
+++ js/main5.js
@@ -1,4 +1,3 @@
-// Funtions
 function printMult(num1, num2) {
     var num3 = num1 * num2;
     console.log(num3);
@@ -9,21 +8,6 @@ function getMult(num1, num2) {
     return num3;
 }
 
-function getMinimum(num) {
-    var minNumber = 10;
-    var minIndex = 0;
-    for (var i = 0; i < num.length; i++) {
-        console.log(num[i]);
-
-        if (num[i] < minNumber) {
-            minNumber = num[i];
-            minIndex = i;
-        }
-    }
-
-    return [minNumber, minIndex];
-}
-
 function getSum(nums) {
     var sum = 0;
     for (var i = 0; i < nums.length; i++) {
@@ -31,8 +15,6 @@ function getSum(nums) {
     }
     return sum;
 }
-
-// Code
 
 // 1
 printMult(2, 17);
@@ -57,11 +39,20 @@ for (var i = 0; i < names.length; i++) {
 }
 
 // 4
+var minIndex = 0;
 var num = [3, 6, 2, 8, 3, 9];
-var min = getMinimum(num);
+var minNumber = 10;
+for (var i = 0; i < num.length; i++) {
+    console.log(num[i]);
 
-console.log("Min number is: " + min[0]);
-console.log('The place is :' + min[1]);
+    if (num[i] < minNumber) {
+        minNumber = num[i];
+        minIndex = i;
+    }
+}
+
+console.log(" min number is: " + minNumber);
+console.log('The place is :' + minIndex);
 
 // 5
 var c = getSum(num);
