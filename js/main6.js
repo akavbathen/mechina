f952cd088f762d2164bb72d9f1f3d04583322af1
progovoy <88984920+akavbathen@users.noreply.github.com>
2021-09-05 00:03:18 +0300

    1 extra2

---
+++ js/main6.js
@@ -0,0 +1,17 @@
+// 1
+var per = {
+    name: 'bathen',
+    age: 33,
+    address: 'nissim aloni 1',
+    floor: 6,
+
+}
+console.log(per);
+
+per.name = "*" + per.name + "*"
+console.log(per.name);
+
+console.log(per.age);
+per.age = 34;
+
+// 2
